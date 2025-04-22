ac8d6abebaf3fb29b7c55b4f58232092d03175c2
Njire Njeri <njirenjeri@gmail.com>
2025-04-23 00:41:17 +0300

    gallery section added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
 import Title from './components/Title/Title'
 import About from './components/About/About'
+import Campus from './components/Campus/Campus'
 
 const App = () => {
   return (
@@ -15,6 +16,8 @@ const App = () => {
         <Programs/>
         <About/>
         <Title subTitle='gallery' title='campus photos' />
+        <Campus/>
+        <Title subTitle='testimonials' title='what a student says' />
       </div>
     </div>
   )
