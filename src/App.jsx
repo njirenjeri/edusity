76a5d17b26e478694a9b87db82e4a6d72875e4ae
Njire Njeri <njirenjeri@gmail.com>
2025-04-22 22:36:23 +0300

    add the programs component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from './components/NavBar/NavBar'
 import Hero from './components/Hero/Hero'
 import Programs from './components/Programs/Programs'
+import Title from './components/Title/Title'
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <NavBar/>
       <Hero/>
       <div className="container">
+        <Title subTitle='our programs' title='what we offer'/>
         <Programs/>
       </div>
     </div>
